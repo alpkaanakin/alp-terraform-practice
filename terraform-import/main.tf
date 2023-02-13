27dbb8267d80e2004a37c278d81d1f2377a89e00
kaan akin <kaan.akin@kloia.com>
2023-02-13 19:59:49 +0300

    import infrastructure to terraform

--- terraform-import/main.tf
+++ terraform-import/main.tf
@@ -1,10 +1,40 @@
-# Local .terraform directories
-**/.terraform/*
+resource "aws_vpc" "imported_vpc" {
+  cidr_block =  "10.0.0.0/16"
+  tags = {
+    Name = "my-import-vpc"
+  }
+}
 
-# .tfstate files
-*.tfstate
-*.tfstate.*
+resource "aws_instance" "imported-EC2"{
 
-# Crash log files
-crash.log
-crash.*.log
+ami = "ami-06c39ed6b42908a36"
+instance_type = "t2.micro"
+subnet_id = aws_subnet.public-subnet-1a.id
+key_name = "EC2-Hack"
+
+
+
+}
+
+
+resource "aws_subnet" "public-subnet-1a" {
+  vpc_id = aws_vpc.imported_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "eu-central-1a"
+
+    tags = {
+    Name = "public-subnet-a1"
+  }
+
+}
+
+resource "aws_route_table" "routeTable" {
+  vpc_id = aws_vpc.imported_vpc.id
+
+    tags = {
+    Name = "public-route-table"
+  }
+  
+
+}
+
