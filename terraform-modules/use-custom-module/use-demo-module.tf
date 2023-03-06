967004e558ec9462de70135cab5000e850f4fac6
kaan akin <kaan.akin@kloia.com>
2023-03-06 19:56:00 +0300

    change use-demo-module-subnets

--- terraform-modules/use-custom-module/use-demo-module.tf
+++ terraform-modules/use-custom-module/use-demo-module.tf
@@ -12,8 +12,8 @@ module "Networking" {
     aws_region = "eu-central-1"
     name = "demo"
     vpc_cidr = "1.0.0.0/16"
-    public_subnets = ["1.0.1.0/24","1.0.2.0/24","1.0.3.0/24"]
-    private_subnets = ["1.0.4.0/24","1.0.5.0/24","1.0.6.0/24"]
+    public_subnets = ["1.0.1.0/24","1.0.2.0/24"]
+    private_subnets = ["1.0.4.0/24","1.0.5.0/24"]
     availability_zones = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
     
 
